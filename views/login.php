df09565db6faab06212fdef95e8c0a5b9f1ef6d8
OussamaBenoujja <osama2code79@gmail.com>
2024-12-27 22:57:32 +0100

    updated the login page by add redirection on user role

--- views/login.php
+++ views/login.php
@@ -4,6 +4,17 @@
 <?php
 session_start();
 
+
+
+
+if(isset($_SESSION['user_id'])){
+      if($_SESSION['role'] == 'admin'){
+        header('Location: admin_dashboard.php');
+      }else if($_SESSION['role'] == 'member'){
+        header('Location: home.php');
+      }
+}
+
 require_once('../control/db_config.php');
 require_once('../control/basecrud.php');
 require_once('../control/control.php');
