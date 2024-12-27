82594e2817b0730a70e06dcfcdb6012ecebf8fb9
OussamaBenoujja <osama2code79@gmail.com>
2024-12-27 22:55:42 +0100

    updated activity page

--- views/activity.php
+++ views/activity.php
@@ -1,5 +1,15 @@
 <?php
+
 session_start();
+
+
+
+if(isset($_SESSION['user_id']) && $_SESSION['role'] == 'admin'){
+        header('Location: admin_dashboard.php');
+}
+
+
+
 require_once('../control/db_config.php');
 require_once('../control/basecrud.php');
 require_once('../control/control.php');
