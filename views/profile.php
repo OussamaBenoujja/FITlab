fb47c5c8d86f58f1e6353c97ff6673d77959fa97
OussamaBenoujja <osama2code79@gmail.com>
2024-12-27 22:57:53 +0100

    added redirection for profile and register

--- views/profile.php
+++ views/profile.php
@@ -1,4 +1,12 @@
 <?php
+
+
+if(isset($_SESSION['user_id']) || $_SESSION['role'] == 'admin'){
+        header('Location: admin_dashboard.php');
+}
+
+
+
 require_once('../control/db_config.php');
 require_once('../control/basecrud.php');
 require_once('../control/control.php');
